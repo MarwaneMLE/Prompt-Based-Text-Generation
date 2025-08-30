296a54cb50bf9b23b2049c8b3718ece7a84622c0
MarwaneMLE <khmarwane10@gmail.com>
2025-08-30 11:10:41 +0100

    capstone project Part 3

---
+++ text_generation/genai/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
+
+'''class CustomUser(AbstractUser):
+    # Add custom fields here if needed
+    pass'''
+
+# Create your models here.
+class Chat(models.Model):
+    user = models.ForeignKey(User,  on_delete=models.CASCADE)
+    message = models.TextField()
+    response = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}: {self.message}"
